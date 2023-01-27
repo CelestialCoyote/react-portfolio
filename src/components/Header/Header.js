8152fee28b15c894e59808bb5aca4ff8cb47ddb3
Paul Stearns <celestialcoyote@gmail.com>
2023-01-26 19:37:21 -0800

    Work on Header component.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,27 @@
 import React from "react";
+import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
+import ArthurDent from '../../assets/ArthurDent.png';
 import './Header.css';
 
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header>
+            <div className="container header-container">
+                <h5>Hello I'm </h5>
+                <h1>Arthur Dent</h1>
+                <h5 className="text-light">Full Stack Develop</h5>
+                
+                <CTA />
+                <HeaderSocials />
+                
+                <div className="arthur-dent">
+                    <img src={ArthurDent} alt="Arthur Dent" />
+                </div>
+                <a href="#contact" className="scroll-down">Scroll Down</a>
+            </div>
+        </header>
     );
 };
 
--- webpack.common.js
+++ webpack.common.js
@@ -21,7 +21,17 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
-            }
+            }, {
+                test: /\.(png|svg|jpg|gif|pdf)$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: '[name].[ext]'
+                    }
+                  }
+                ]
+              }
         ]
     },
     plugins: [
